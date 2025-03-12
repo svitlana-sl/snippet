f3ae2198b0d7a61101f2a062debda5303d950fcc
svitlana-sl <svitlana.slobodeniuk@gmail.com>
2025-03-12 12:15:41 +0100

    Refactor server initialization and enhance EJS configuration for dashboard rendering

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,15 @@
 import "dotenv/config";
 import cors from "cors";
 import express from "express";
+import mongoose from "mongoose";
+import path from "path";
+import { fileURLToPath } from "url";
 import { notFound } from "./controllers/notFoundController";
 import snippetRoutes from "./routes/snippetRoutes";
-import mongoose from "mongoose";
+
+// __dirname for ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Variables
 const app = express();
@@ -14,20 +20,31 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// EJS config
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// Route for dashboard.ejs render
+app.get("/", (req, res) => {
+  res.render("dashboard");
+});
+
 // Routes
 app.use("/api/snippets", snippetRoutes);
 app.all("*", notFound);
 
 // Database connection
-try {
-  await mongoose.connect(process.env.MONGO_URI!);
-  console.log("Database connection OK");
-} catch (err) {
-  console.error(err);
-  process.exit(1);
-}
+mongoose
+  .connect(process.env.MONGO_URI!)
+  .then(() => {
+    console.log("Database connection OK");
 
-// Server Listening
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}! 🚀`);
-});
+    // Server Listening
+    app.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}! 🚀`);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
