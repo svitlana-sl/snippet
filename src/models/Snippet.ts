3f11a7dda4ca5254de8e37ba941f1229e70e5092
svitlana-sl <svitlana.slobodeniuk@gmail.com>
2025-03-12 10:51:46 +0100

    add Snippet model and update mongoose dependency; include ejs in package.json

---
+++ src/models/Snippet.ts
@@ -0,0 +1,24 @@
+import { Schema, model, Document } from "mongoose";
+
+export interface ISnippet extends Document {
+  title: string;
+  code: string; //saved in base64
+  language: string;
+  tags: string[];
+  createdAt: Date;
+  updatedAt: Date;
+  expiresAt?: Date; // date when snippet expires (if required)
+}
+
+const snippetSchema = new Schema<ISnippet>(
+  {
+    title: { type: String, required: true },
+    code: { type: String, required: true },
+    language: { type: String, required: true },
+    tags: [{ type: String }],
+    expiresAt: { type: Date },
+  },
+  { timestamps: true }
+);
+
+export default model<ISnippet>("Snippet", snippetSchema);
